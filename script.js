400e5e8e93b969ebb830060da345ff15ab79e4ac
Jeanne Allen <112727701+tebazele@users.noreply.github.com>
2023-07-17 21:32:09 -0600

    added notes for students

--- script.js
+++ script.js
@@ -1,53 +1,73 @@
 // 🧩What is data? The answers to questions
 
+// This is a comment
+/* This is a multi-line
+comment */
+
 // There are three basic data types in javascript
+
+// string (don't forget the double quote marks)
 "Hello there"
 "Go!"
 "Jeanne"
 "An entire sentence or two. I like strings! Woo hoo!"
-
+// number
 4
 590
 321
 100000000000000000000000000000
 
-
+// boolean
 true
 false
 
+/* This is how we declare variables; we always start with the declaration on the left and the value on the right. 
+This is called left-hand assignment */
 let myName = "Jeanne";
 let myAge = 38
 let myGrade = 1000000000
 let wearsGlasses = true
 
+// we can declare variables that are undefined and save values into them later
 let total;
 // undefined
 
+
+// If we want a variable to be nothing, we set it to null
 let myNothingVariable = null;
 
-// truthy values: true, everything else
+// All values in javascript are either "truthy" or "falsy"
 
 // falsy value: undefined, false, null, 0
+// truthy values: true, everything else
+
 let greeting = "How's it going?"
 
-// print stuff to the console
-// console.log("Hello " + myName + ". " + greeting)
-// console.log(myAge, myGrade, wearsGlasses)
-// console.log("supercalifragalisticsexpialodocious".length)
+// Let's print stuff to the console
+console.log("Hello " + myName + ". " + greeting)
+console.log(myAge, myGrade, wearsGlasses)
+console.log("supercalifragalisticsexpialodocious".length)
 
-// we are birdwatching
+// We can use javascript to do math. Here, we are birdwatching. How many birds did we see?
 
 let ducks = 7;
 let geese = 2;
 let robins = 14;
 
+// addition
 let totalBirds = 7 + 2 + 14;
 
 console.log("This is the total birds I saw", totalBirds)
 
-let subtractionAnswer = 789 - 355;
+// These are mathematical operators in javascript
+
+// subtract
+let subtractionAnswer = 789 - 355; 
+// multiply
 let multiplicationAnswer = 55 * 90;
+// divide
 let divisionAnswer = 100 / 10;
+// get the remainder
 let modulusAnswer = 9 % 2;
 
 // the modulus operater refers to remainders after doing division
@@ -55,6 +75,7 @@ let modulusAnswer = 9 % 2;
 
 // Control flows
 
+// Conditionals (if/else statements)
 if(3 > 2) {
     console.log("three is greater than two!")
 }
